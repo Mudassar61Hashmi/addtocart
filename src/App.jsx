31ce7358521207a08bbc4370235072adff147953
unknown <hashmig48@gmail.com>
2023-10-15 21:39:26 -0700

    This is new repo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Cart from './components/Cart';
+import './components/cart.css';
+const App = () => {
+  return (
+    <>
+    <Cart/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Items.jsx
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react';
+import { CartContext } from './Cart';
+const Items = ({id, title, description, price, img, quantity}) => {
+ const {removeItem, increment, decrement}=useContext(CartContext);
+  return (
+    <>
+                      <div className="items-info">
+<div className="product-img">
+    <img src={img} alt="S21" />
+</div>
+<div className="title">
+    <h2>{title}</h2>
+    <p>{description}</p>
+
+</div>
+
+<div className="add-minus-quantity">
+<i className="fa fa-minus minus" onClick={()=>decrement(id)}></i>
+<input type="text" placeholder={quantity} />
+<i className="fa fa-plus add" onClick={()=>increment(id)}></i>
+</div>
+
+<div className="price">
+    <h3>₨-/ {price}</h3>
+</div>
+
+<div className="remove-item">
+<i className="fa fa-trash-alt remove" onClick={()=>removeItem(id)}></i>
+</div>
+                </div>
+                <hr />
+    </>
+  )
+}
+
+export default Items;
